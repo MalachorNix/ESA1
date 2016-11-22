0006b0306a0b36b3253e0d961aac8aee1ae864a5
Pavel Fadeev <malachornix@gmail.com>
2016-11-22 23:34:17 +0400

    Trying to add Entity Persistence Support

--- src/main/java/com/dcpro/dao/GenericDAOImpl.java
+++ src/main/java/com/dcpro/dao/GenericDAOImpl.java
@@ -1,14 +1,19 @@
 package com.dcpro.dao;
 
 import com.dcpro.util.HibernateUtil;
-import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.exception.ConstraintViolationException;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceContext;
 import java.io.Serializable;
 import java.util.List;
 
 public abstract class GenericDAOImpl<T, PK extends Serializable> implements GenericDAO {
+
+    @PersistenceContext(unitName = "sample")
+    private EntityManager entityManager;
 
     protected Session getSession() {
         return HibernateUtil.getSessionFactory().openSession();
@@ -16,34 +21,45 @@ public abstract class GenericDAOImpl<T, PK extends Serializable> implements Gene
 
     @Override
     public void save(Object entity) {
-        Session session = this.getSession();
+        /*Session session = this.getSession();
         session.beginTransaction();
         session.save(entity);
         session.getTransaction().commit();
-        session.close();
+        session.close();*/
+        EntityTransaction transaction = entityManager.getTransaction();
+        entityManager.persist(entity);
+        transaction.commit();
     }
 
     @Override
     public void update(Object entity) {
-        Session session = this.getSession();
+        /*Session session = this.getSession();
         session.beginTransaction();
         session.update(entity);
         session.getTransaction().commit();
-        session.close();
+        session.close();*/
+//        entityManager.refresh(entity);
+        EntityTransaction transaction = entityManager.getTransaction();
+        entityManager.refresh(entity);
+        transaction.commit();
     }
 
     @Override
     public void merge(Object entity) {
-        Session session = this.getSession();
+        /*Session session = this.getSession();
         session.beginTransaction();
         session.merge(entity);
         session.getTransaction().commit();
-        session.close();
+        session.close();*/
+//        entityManager.merge(entity);
+        EntityTransaction transaction = entityManager.getTransaction();
+        entityManager.merge(entity);
+        transaction.commit();
     }
 
     @Override
     public void delete(Object entity) throws ConstraintViolationException{
-        Session session = null;
+        /*Session session = null;
         try {
             session = this.getSession();
             session.beginTransaction();
@@ -53,17 +69,24 @@ public abstract class GenericDAOImpl<T, PK extends Serializable> implements Gene
             if (session != null) {
                 session.close();
             }
-        }
+        }*/
+//        entityManager.remove(entity);
+        EntityTransaction transaction = entityManager.getTransaction();
+        entityManager.remove(entity);
+        transaction.commit();
     }
 
     @Override
     public List findAll(Class clazz) {
-        Session session = this.getSession();
+        /*Session session = this.getSession();
         session.beginTransaction();
         Query query = session.createQuery("from " + clazz.getName());
         List list = query.list();
         session.getTransaction().commit();
-        session.close();
+        session.close();*/
+        EntityTransaction transaction = entityManager.getTransaction();
+        List list = entityManager.createQuery("from " + clazz.getName()).getResultList();
+        transaction.commit();
         return list;
     }
 }
--- src/main/java/com/dcpro/dao/GroupDAOImpl.java
+++ src/main/java/com/dcpro/dao/GroupDAOImpl.java
@@ -4,17 +4,30 @@ import com.dcpro.entities.Group;
 import org.hibernate.Query;
 import org.hibernate.Session;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceContext;
+
 public class GroupDAOImpl extends GenericDAOImpl implements GroupDAO {
+
+    @PersistenceContext(unitName = "sample")
+    private EntityManager entityManager;
 
     @Override
     public Group getByNumberAndFaculty(int groupNumber, String faculty) {
-        Session session = getSession();
-        session.beginTransaction();
-        Query query = session.createQuery("from Group where groupNumber = :num and faculty = :faculty");
-        query.setLong("num", groupNumber);
-        query.setString("faculty", faculty);
-        Group group = (Group) query.uniqueResult();
-        session.getTransaction().commit();
+//        Session session = getSession();
+//        session.beginTransaction();
+//        Query query = session.createQuery("from Group where groupNumber = :num and faculty = :faculty");
+        EntityTransaction transaction = entityManager.getTransaction();
+        javax.persistence.Query query = entityManager
+                .createQuery("from Group where groupNumber = :num and faculty = :faculty");
+        query.setParameter("num", groupNumber);
+        query.setParameter("faculty", faculty);
+        Group group = (Group) query.getSingleResult();
+//        query.setLong("num", groupNumber);
+//        query.setString("faculty", faculty);
+//        Group group = (Group) query.uniqueResult();
+//        session.getTransaction().commit();
         return group;
     }
 }
